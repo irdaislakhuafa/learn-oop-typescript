148a81a2bdc4afabc7d7d58e77f2e873e6f8b75d
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2024-05-28 15:21:06 +0700

    feat: implement class method overriding (#9)
    * feat: implement class method overriding
    
    This commit introduces unit tests for class method overriding.
    
    This will allow developers to override methods in derived classes and ensure that the overridden methods are called when the derived class is instantiated.
    
    * docs: add dist to .gitignore
    
    Adds dist to .gitignore to prevent accidentally committing built files.

---
+++ tests/class-method-overriding.test.ts
@@ -0,0 +1,33 @@
+describe('class method overriding', () => {
+	class Employee {
+		name: string
+
+		constructor(v: { name: string }) {
+			this.name = v.name
+		}
+
+		sayHello(name: string): string {
+			return `Hello ${name}, my name is ${this.name}`
+		}
+	}
+
+	class Manager extends Employee {
+		constructor(name: string) {
+			super({ name })
+		}
+
+		sayHello(name: string): string {
+			return `Hello ${name}, my name is ${this.name} and i'm is Manager`
+		}
+	}
+
+	it('learn method overriding', () => {
+		const employee = new Employee({ name: "a" })
+		expect(employee.name).toBe('a')
+		expect(employee.sayHello('b')).toBe('Hello b, my name is a')
+
+		const manager = new Manager('c')
+		expect(manager.name).toBe('c')
+		expect(manager.sayHello(employee.name)).toBe("Hello a, my name is c and i'm is Manager")
+	})
+})
