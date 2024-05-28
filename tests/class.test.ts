ebc3f4456fc3f86c66abbeab100515c9b86b709f
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-05-28 13:27:34 +0700

    feat(class): add class example
    Add class example for learning purpose
    - Create Customer and Order class
    - Initialize each class

---
+++ tests/class.test.ts
@@ -0,0 +1,10 @@
+describe('class', () => {
+	it('learn class', () => {
+		// create class
+		class Customer { }
+		class Order { }
+
+		const customer: Customer = new Customer()
+		const order: Order = new Order()
+	})
+})
