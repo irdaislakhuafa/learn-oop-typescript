df68a4967f5013db6671180f686ae5f6b0f955d1
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-05-29 00:25:21 +0700

    feat: add static method to MathUtil
    Adds a static method to MathUtil that sums a variable number of integers.
    
    BREAKING CHANGE: The MathUtil class now has a static method sum.

--- tests/static-properties.test.ts
+++ tests/static-properties.test.ts
@@ -5,9 +5,22 @@ describe('static', () => {
 		static AUTHOR: string = "Irda Islakhu Afa"
 	}
 
+	class MathUtil {
+		static sum(...values: number[]): number {
+			let total: number = 0;
+			values.forEach(v => total += v)
+			return total
+		}
+	}
+
 	it('learn static properties', () => {
 		expect(Configuration.NAME).toBe("Learning TypeScript OOP")
 		expect(Configuration.VERSION).toBe("v0.0.1")
 		expect(Configuration.AUTHOR).toBe("Irda Islakhu Afa")
+	})
+
+	it('learn static method', () => {
+		const result = MathUtil.sum(1, 2, 3, 4, 5)
+		expect(result).toBe(15)
 	})
 })
